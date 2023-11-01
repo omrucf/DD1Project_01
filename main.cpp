e7ce5682fa39a270ea5524fbc97e11ffbe166fcd
omrucf <omrucf@aucegypt.edu>
2023-11-01 10:34:53 +0200

    removed test cases

--- main.cpp
+++ main.cpp
@@ -9,83 +9,74 @@ int main(int argc, char *argv[])
     string BooleanExpressionInput = "";
     vector<minterms> PI;
 
-    string testCases[10] = {"A+B", "A*B", "A+B+C", "A*B*C+(!B)", "A+B+C+D", "(!A*B)+(C*!D)", "A*B+CD+D*E", "A*B*C*D+D*E", "A+B+C+D+E+F", "(A*!B*C)+D+E*F"};
-
     cout << "NOTE : AND = *, OR = +, NOT = !" << endl;
     cout << "NOTE : Include ( ) When Negating" << endl;
-    // cout << "Enter a Boolean Expression : ";
-    //
+    cout << "Enter a Boolean Expression : ";
+    getline(cin, BooleanExpressionInput);
 
-
-    for (int i = 0; i < 10; i++)
+    if (BooleanExpressionInput.empty())
     {
 
-        cout << "Case f = " << testCases[i] << endl;
-        BooleanExpressionInput = testCases[i];
-        if (BooleanExpressionInput.empty())
+        cout << "ERROR : No Boolean Expression Entered!" << endl;
+    }
+    else if (isValidBooleanExpression(BooleanExpressionInput) && containsOperator(BooleanExpressionInput))
+    {
+        cout << "---------------------------------------" << endl;
+        removeDoubleNegations(BooleanExpressionInput);
+        map<char, string> stringCharPairs = MakePairs(BooleanExpressionInput);
+        evaluateExpression(BooleanExpressionInput, stringCharPairs);
+        string SOP = canonicalSOP(stringCharPairs);
+        string POS = canonicalPOS(stringCharPairs);
+        for (auto &x : stringCharPairs)
         {
-
-            cout << "ERROR : No Boolean Expression Entered!" << endl;
+            cout << x.first << "\t";
         }
-        else if (isValidBooleanExpression(BooleanExpressionInput) && containsOperator(BooleanExpressionInput))
+
+        // Print table rows
+        int numRows = stringCharPairs.begin()->second.length();
+        for (int i = numRows; i >= 0; i--)
         {
-            cout << "---------------------------------------" << endl;
-            removeDoubleNegations(BooleanExpressionInput);
-            map<char, string> stringCharPairs = MakePairs(BooleanExpressionInput);
-            evaluateExpression(BooleanExpressionInput, stringCharPairs);
-            string SOP = canonicalSOP(stringCharPairs);
-            string POS = canonicalPOS(stringCharPairs);
             for (auto &x : stringCharPairs)
             {
-                cout << x.first << "\t";
+                cout << x.second[i] << "\t";
             }
 
-            // Print table rows
-            int numRows = stringCharPairs.begin()->second.length();
-            for (int i = numRows; i >= 0; i--)
-            {
-                for (auto &x : stringCharPairs)
-                {
-                    cout << x.second[i] << "\t";
-                }
-
-                cout << endl;
-            }
-            cout << "---------------------------------------" << endl;
-            cout << "Canonical SOP : " << SOP << endl;
-            cout << "Canonical POS : " << POS << endl;
-            cout << "---------------------------------------" << endl;
-
-            vector<string> mntrms = toBinary(SOP);
-            PI = PIGenerator(mntrms);
-
-            for (int i = 0; i < PI.size(); i++)
-            {
-                cout << PI[i].binRep << "\tdec:\t";
-                for (auto it = PI[i].decimals.begin(); it != PI[i].decimals.end(); it++)
-                {
-                    cout << *it << " ";
-                }
-                cout << endl;
-            }
-
-            vector<int> onset = setOnset(PI);
-            // remove duplicates from onset and sort
-
-            sort(onset.begin(), onset.end());
-            onset.erase(unique(onset.begin(), onset.end()), onset.end());
-
-            vector<minterms> EPIs = findEPIs(PI, onset);
-
-            KarnaughMap(stringCharPairs, BooleanExpressionInput);
-
-            // printKMap(onset);
+            cout << endl;
         }
-        else
+        cout << "---------------------------------------" << endl;
+        cout << "Canonical SOP : " << SOP << endl;
+        cout << "Canonical POS : " << POS << endl;
+        cout << "---------------------------------------" << endl;
+
+        vector<string> mntrms = toBinary(SOP);
+        PI = PIGenerator(mntrms);
+
+        for (int i = 0; i < PI.size(); i++)
         {
-
-            cout << "ERROR : Invalid Boolean Expression!" << endl;
+            cout << PI[i].binRep << "\tdec:\t";
+            for (auto it = PI[i].decimals.begin(); it != PI[i].decimals.end(); it++)
+            {
+                cout << *it << " ";
+            }
+            cout << endl;
         }
+
+        vector<int> onset = setOnset(PI);
+        // remove duplicates from onset and sort
+
+        sort(onset.begin(), onset.end());
+        onset.erase(unique(onset.begin(), onset.end()), onset.end());
+
+        vector<minterms> EPIs = findEPIs(PI, onset);
+
+        KarnaughMap(stringCharPairs, BooleanExpressionInput);
+
+        // printKMap(onset);
+    }
+    else
+    {
+
+        cout << "ERROR : Invalid Boolean Expression!" << endl;
     }
 
     // vector<int> onset = setOnset(PI);
